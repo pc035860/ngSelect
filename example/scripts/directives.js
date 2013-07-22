8e0bccc73882a708ee2ced0fcea9890480e05895
pc035860 <pc035860@gmail.com>
2013-07-22 15:33:34 +0800

    code-highlight directive improvements

--- example/scripts/directives.js
+++ example/scripts/directives.js
@@ -5,18 +5,35 @@ angular.module('exampleApp')
 function ($timeout) {
   return {
     restrict: 'EA',
-    template: '<pre><code></code></pre>',
-    link: function(scope, iElm, iAttrs) {
-      scope.$watch(iAttrs.code, function (newCode, oldCode) {
-        if (angular.isDefined(newCode) && newCode !== oldCode) {
-          var codeElm = iElm.find('code');
-          codeElm.text(newCode);
+    compile: function(tElm, tAttrs, transclude) {
+      // get static code
+      // strip the starting "new line" character
+      var staticCode = tElm[0].innerHTML.replace(/^\r\n|\r|\n/, '');
+
+      // put template
+      tElm.html('<pre><code></pre></code>');
+
+      return function postLink(scope, iElm, iAttrs) {
+        var codeElm = iElm.find('code');
+
+        if (staticCode) {
+          _highlightCode(codeElm, staticCode);
+        }
+
+        scope.$watch(iAttrs.code, function (newCode, oldCode) {
+          if (angular.isDefined(newCode) && newCode !== oldCode) {
+            _highlightCode(codeElm, newCode);
+          }
+        });
+
+        function _highlightCode(codeElm, code) {
+          codeElm.text(code);
 
           $timeout(function () {
             hljs.highlightBlock(codeElm[0]);
           });
         }
-      });
+      };
     }
   };
 }]);
